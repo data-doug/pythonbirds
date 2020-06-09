18f80faacf515c2107717dc9c09de3d683d204fe
douglas.dias <douglasdiasalmeida@gmail.com>
2020-06-09 20:43:57 -0300

    Criado método cumprimentar

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,7 +1,17 @@
 class Pessoa:
+
+    def __init__(self, *filhos, nome = None, idade = 35):
+        self.nome = nome
+        self.idade = idade
+        self.filhos = list(filhos)
+
+
     def cumprimentar(self):
         return "Olá"
 
 if __name__ == '__main__':
-    p = Pessoa()
-    print(p.cumprimentar())
+    douglas = Pessoa(nome="Douglas")
+    edson = Pessoa(douglas, nome="Edson")
+
+    for filho in edson.filhos:
+        print(filho.nome)
